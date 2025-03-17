de18c88a317580e0d3597e56352aaf3580ed9221
Avinash Guleria <avinashguleria1009@gmail.com>
2025-03-17 18:46:57 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import LandingPage from "./LandingPage"
-
+import Chessmoment from "../component/Chessmoment"
+import HvsM from '../assests/hikaru vs magnus.jpg'
 
 function App() {
+
+  const dummyData = {
+    move: "Qg7# - The Blunder of the Century",
+    players: "Magnus Carlsen vs. Hikaru Nakamura",
+    description: "Hikaru hung his queen in a winning position and lost instantly!",
+    image: HvsM,
+  };
 
   return (
     <>
       <LandingPage/>
+      <Chessmoment {...dummyData} />
     </>
   )
 }
