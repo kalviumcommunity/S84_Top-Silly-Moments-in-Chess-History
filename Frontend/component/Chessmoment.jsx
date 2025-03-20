f31d059cc2dfca70897dfb4ce8cd1fccce45ffd9
Avinash Guleria <avinashguleria1009@gmail.com>
2025-03-20 11:36:32 +0530

    connected backend to frontend

--- Frontend/component/Chessmoment.jsx
+++ Frontend/component/Chessmoment.jsx
@@ -1,15 +1,16 @@
+import '../styles/Chessmoment.css'
 
 function Chessmoment({ image, move, player, description}){
 
     return(
-        <>
-        <div style={{display: "flex", alignItems: "center", justifyContent: "center", color: "white"}}>
+        <div className='chessmoment__parent'>
+        <div className="Chessmoment__box">
             <img style={{width: "400px"}} src={image} alt="Chess moment" />
+            <h2 className='chessmoment__content'> {move} </h2>
+            <h5 className='chessmoment__content'> {player} </h5>
+            <p className='chessmoment__content'> {description} </p>
         </div>
-            <h2> {move} </h2>
-            <h5> {player} </h5>
-            <p> {description} </p>
-        </>
+        </div>
     )
 }
 
